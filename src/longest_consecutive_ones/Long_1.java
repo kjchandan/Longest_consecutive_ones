d61265211c1e7c4db356f4188a4ae79c0bb06ae1
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-17 14:51:57 +0530

    commit

--- src/longest_consecutive_ones/Long_1.java
+++ src/longest_consecutive_ones/Long_1.java
@@ -7,14 +7,14 @@ public class Long_1 {
 		Scanner scan = new Scanner(System.in);
 		
 		int n = scan.nextInt();
-		int count =0;
+		int count =0;  //Take count as 0
 		
-		while(n>0) 
+		while(n>0)   //loop untill n is 0.
 		{
-		n = n & n<<1;
-		count++;
+		n = n & n<<1;  // check if n and left shift of n is not eqal to zer and then give to assign the value to n.
+		count++;      //If it is not eqal to zero then increment count . 
 		}
-		System.out.println(count);
+		System.out.println(count);// soo we eill get eh consecutivte ones as many as count.
 	}
 
 }
